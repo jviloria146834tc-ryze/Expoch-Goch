792e04a7c7fb73451eaefa69dcc3010d934f3e9d
Jay Lloyd Viloria <j.viloria.146834.tc@umindanao.edu.ph>
2026-09-12 18:00:46 +0800

    Restructured repo

--- MyApp3/App.js
+++ MyApp3/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hello, Jhanalyn! Dedto kah MaNgutana sa porpis dol.,, 
+      <Text> Hello, JIRLINN! Dedto kah MaNgutana sa porpis dol.,, 
          ayaw kog Gara garae dol.,,, ego ka ani Akung kinumo Henuktok 
          Jod Kah.,,,Ayaw seg...gAragara....dile ko madAla komedya...Ga 
          yama yama naka deha bossing..,,, ma sampolan taka sakong kinomo 
